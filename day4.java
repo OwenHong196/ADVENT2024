2f506d350445a40f5b82a7e8513d4ae46b7a558e
BT_4N2_10 <sample@bths.edu>
2024-12-09 13:17:10 -0500

    day4part1 done

--- day4.java
+++ day4.java
@@ -3,20 +3,34 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class day4 {
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("input.txt");
         String[][] xmas = new String[fileData.size()][fileData.get(0).length()];
+        for(int i = 0; i < fileData.size(); i++){
+            for(int j = 0; j < fileData.get(0).length(); j++){
+                xmas[i][j]=fileData.get(i).substring(j,j+1);
+            }
+        }
+        int count = 0;
         for (int i = 0; i < xmas.length; i++){
             for(int j = 0; j < xmas[i].length; j++){
-                if(j < xmas.length - 4){
-                    if(xmas[i][j].equals("X")){
+                if()
+                //down down
+                //down up
+                //up up
+                //up down
 
-                    }
+                //down down
+                try{
+
                 }
+                catch (ArrayIndexOutOfBoundsException a) { }
             }
         }
     }
+
 
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
@@ -34,4 +48,107 @@ public class day4 {
             return fileData;
         }
     }
+
+    public static void part1(){
+        ArrayList<String> fileData = getFileData("input.txt");
+        String[][] xmas = new String[fileData.size()][fileData.get(0).length()];
+        for(int i = 0; i < fileData.size(); i++){
+            for(int j = 0; j < fileData.get(0).length(); j++){
+                xmas[i][j]=fileData.get(i).substring(j,j+1);
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < xmas.length; i++){
+            for(int j = 0; j < xmas[i].length; j++){
+                //right
+                try {
+                    if (xmas[i][j].equals("X")
+                            && xmas[i][j+1].equals("M")
+                            && xmas[i][j+2].equals("A")
+                            && xmas[i][j+3].equals("S"))
+                        count ++;
+                }
+                catch (ArrayIndexOutOfBoundsException a) { }
+
+
+
+                //left
+                try{
+                    if (xmas[i][j].equals("X")
+                            && xmas[i][j-1].equals("M")
+                            && xmas[i][j-2].equals("A")
+                            && xmas[i][j-3].equals("S")){
+                        count ++;
+                    }
+                }
+                catch (ArrayIndexOutOfBoundsException a) { }
+                //up
+                try{
+                    if (xmas[i][j].equals("X")
+                            && xmas[i-1][j].equals("M")
+                            && xmas[i-2][j].equals("A")
+                            && xmas[i-3][j].equals("S")){
+                        count ++;
+                    }
+                }
+                catch (ArrayIndexOutOfBoundsException a) { }
+                //down
+                try{
+                    if (xmas[i][j].equals("X")
+                            && xmas[i+1][j].equals("M")
+                            && xmas[i+2][j].equals("A")
+                            && xmas[i+3][j].equals("S")){
+                        count ++;
+                    }
+                }
+                catch (ArrayIndexOutOfBoundsException a) { }
+                //diag down right
+                try{
+                    if (xmas[i][j].equals("X")
+                            && xmas[i+1][j+1].equals("M")
+                            && xmas[i+2][j+2].equals("A")
+                            && xmas[i+3][j+3].equals("S")){
+                        count ++;
+                    }
+                }
+                catch (ArrayIndexOutOfBoundsException a) { }
+                //diag down left
+                try{
+                    if (xmas[i][j].equals("X")
+                            && xmas[i+1][j-1].equals("M")
+                            && xmas[i+2][j-2].equals("A")
+                            && xmas[i+3][j-3].equals("S")){
+                        count ++;
+                    }
+                }
+                catch (ArrayIndexOutOfBoundsException a) { }
+                //diag up right
+                try{
+                    if (xmas[i][j].equals("X")
+                            && xmas[i-1][j+1].equals("M")
+                            && xmas[i-2][j+2].equals("A")
+                            && xmas[i-3][j+3].equals("S")){
+                        count ++;
+                    }
+                }
+                catch (ArrayIndexOutOfBoundsException a) { }
+                //diag up left
+                try{
+                    if (xmas[i][j].equals("X")
+                            && xmas[i-1][j-1].equals("M")
+                            && xmas[i-2][j-2].equals("A")
+                            && xmas[i-3][j-3].equals("S")){
+                        count ++;
+                    }
+                }
+                catch (ArrayIndexOutOfBoundsException a) { }
+            }
+        }
+        System.out.println(count);
+    }
 }
+
+
+
+
+
