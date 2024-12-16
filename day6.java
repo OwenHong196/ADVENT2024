40dbb03ba1f611839ecb2d8e7c7826b2f01712a2
BT_4N2_10 <sample@bths.edu>
2024-12-16 13:20:45 -0500

    worked on fixing code

--- day6.java
+++ day6.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class day6 {
+    static boolean up = true;
+    static boolean down = false;
+    static boolean left = false;
+    static boolean right = false;
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("Input.txt");
         String[][] array = new String[fileData.size()][fileData.get(0).length()];
@@ -12,19 +16,44 @@ public class day6 {
                 array[i][j] = fileData.get(i).substring(j,j+1);
             }
         }
-        boolean up = true;
-        boolean down = false;
-        boolean left = false;
-        boolean right = false;
-        for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array[0].length; j++){
-                if(array[i][j].equals("^")){
-                    if (check(up,down,left,right,array,i,j)){
-                        array
-                    }
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                if (up && check(array, i, j) == 1 && array[i][j].equals("^")) {
+                    array[i][j] = "X";
+                    array[i - 1][j] = "^";
+                    i = 0;
+                    j = 0;
+                    count++;
+                }
+                if (right && check(array, i, j) == 1 && array[i][j].equals("^")) {
+                    array[i][j] = "X";
+                    array[i][j + 1] = "^";
+                    i = 0;
+                    j = 0;
+                    count++;
+                }
+                if (down && check(array, i, j) == 1 && array[i][j].equals("^")) {
+                    array[i][j] = "X";
+                    array[i + 1][j] = "^";
+                    i = 0;
+                    j = 0;
+                    count++;
+                }
+                if (left && check(array, i, j) == 1 && array[i][j].equals("^")) {
+                    array[i][j] = "X";
+                    array[i][j - 1] = "^";
+                    i = 0;
+                    j = 0;
+                    count++;
+                }
+                if (check(array, i, j) == 0) {
+                    count++;
+                    break;
                 }
             }
         }
+        System.out.println(count);
     }
 
     public static ArrayList<String> getFileData(String fileName) {
@@ -43,27 +72,59 @@ public class day6 {
             return fileData;
         }
     }
-    public static boolean check(boolean up, boolean down, boolean left, boolean right, String[][] arr,int row, int col){
+
+    public static int check(String[][] arr,int row, int col){
         if (up){
             if (row-1 >= 0 && arr[row-1][col].equals(".")){
-                return true;
+                return 1;
+            }else if (row-1 >= 0 && arr[row-1][col].equals("#")){
+                System.out.println("cant up");
+                changeDir();
+                return 2;
             }
         }
         if (down){
             if (row+1 < arr.length && arr[row+1][col].equals(".")){
-                return true;
+                return 1;
+            }else if (row+1 < arr.length && arr[row+1][col].equals("#")){
+                changeDir();
+                return 2;
             }
         }
         if (left){
             if (col-1 >= 0 && arr[row][col-1].equals(".")){
-                return true;
+                return 1;
+            }else if (col-1 >= 0 && arr[row][col-1].equals("#")){
+                changeDir();
+                return 2;
             }
         }
         if (right){
             if (col+1 >= arr[0].length && arr[row][col+1].equals(".")){
-                return true;
+                return 1;
+            }else if (col+1 >= arr[0].length && arr[row][col+1].equals("#")){
+                changeDir();
+                return 2;
             }
         }
-        return false;
+        return 0;
+    }
+    public static void changeDir(){
+        if (up){
+            up = false;
+            right = true;
+        }
+        if (right){
+            right = false;
+            down = true;
+        }
+        if (down){
+            down = false;
+            left = true;
+        }
+        if (left){
+            left = false;
+            up = true;
+        }
     }
 }
